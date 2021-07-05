1867a6edbac58ea1e80d0a0ff3773d042cb9faff
imzlp <38953266+imzlp@users.noreply.github.com>
2021-07-05 16:24:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ hexo.extend.tag.register('authortables', function (args, content) {
             for(var index=1;index<currentTag.length;index++)
             {
                 if(!(TagValue === ""))
-                    TagValue+=":";
+                TagValue+=":";
                 TagValue += currentTag[index];
             }
             argsObj[TagName] = TagValue;
@@ -91,3 +91,131 @@ hexo.extend.tag.register('authortables', function (args, content) {
     iframes +=`</table>\n</tr>\n`;
     return iframes;
 }, { ends: true });
+
+hexo.extend.tag.register('allcontributor', function (args, content) 
+{
+    let default_avatar = ""
+    let image_size = "80px"
+    let font_icon_color = "555"
+    let font_color = "009bf5"
+    let font_weight = 500
+    let github_avatar = true
+    let board_width = "100px"
+    let margin_rule = "auto"
+    if(!(hexo.config.hexo_contributor === undefined))
+    {
+        if(!(hexo.config.hexo_contributor.default_avatar === null))
+          default_avatar = hexo.config.hexo_contributor.default_avatar;
+        if(!(hexo.config.hexo_contributor.image_size === null))
+            image_size = hexo.config.hexo_contributor.image_size;
+        if(!(hexo.config.hexo_contributor.font_icon_color === null))
+            font_icon_color = hexo.config.hexo_contributor.font_icon_color;
+        if(!(hexo.config.hexo_contributor.font_color === null))
+            font_color = hexo.config.hexo_contributor.font_color;
+        if(!(hexo.config.hexo_contributor.font_weight === null))
+            font_weight = hexo.config.hexo_contributor.font_weight;
+        if(!(hexo.config.hexo_contributor.github_avatar === null))
+            github_avatar = hexo.config.hexo_contributor.github_avatar;
+        if(!(hexo.config.hexo_contributor.board_width === null))
+            board_width = hexo.config.hexo_contributor.board_width;
+        if(!(hexo.config.hexo_contributor.margin_rule === null))
+            margin_rule = hexo.config.hexo_contributor.margin_rule;
+    }
+    let iframes=`<table style="width:auto;margin:${margin_rule};margin-bottom:5px;margin-top:5px;">\n<tr>\n`;
+
+    let authors = new Array();
+    var posts = hexo.locals.get('posts');
+    posts.each(function(post, index) {
+            var authorName = post.author.name
+            console.log(post.title);
+            if(authorName == undefined)
+                return;
+            var found = false;
+            authors.forEach((author)=>{
+                if(authorName == author.name){
+                    author.article_num += 1;
+                    found = true;
+                }
+            });
+            if(!found){
+                var newAuthor = {
+                    name: "",
+                    github_id: "",
+                    avatar_image: "",
+                    site_addr: "",
+                    email: "",
+                    zhihu_addr: "",
+                    wechat_image: "",
+                    article_num: 1
+                }
+                newAuthor.name = post.author.name;
+                newAuthor.github_id = post.author.github_id;
+                newAuthor.avatar_image = post.author.avatar_image;
+                newAuthor.site_addr = post.author.site_addr;
+                newAuthor.email = post.author.email;
+                newAuthor.zhihu_addr = post.author.zhihu_addr;
+                newAuthor.wechat_image = post.author.wechat_image;
+                authors.push(newAuthor);
+                console.log(authors);
+            }
+    });
+
+    authors.sort(function(a,b){return a.article_num < b.article_num;});
+
+    var result = [];
+    for(var i=0,len=authors.length;i<len;i+=7){
+       result.push(authors.slice(i,i+7));
+    }
+    result.forEach(function(authors, index) {
+        iframes += `<table style="width:auto;margin:${margin_rule};margin-bottom:5px;margin-top:5px;"><tr>`;
+        authors.forEach(function(post, index) 
+        {
+            // console.log(post);
+            var article_num = post.article_num;
+            var authorName = post.name;
+            var githubName = post.github_id;
+            var authorLink = post.site_addr;
+            var authorAvatar = ""
+            if(post.avatar_image == null || post.avatar_image === "")
+            {
+                if(!(githubName === "") && github_avatar)
+                {
+                authorAvatar = "https://avatars.githubusercontent.com/"+githubName
+                }
+                else
+                {
+                var authorAvatar = default_avatar;
+                }
+            }
+            else
+            {
+                authorAvatar = post.avatar_image;
+            }
+            var authorEmail = post.email;
+            var authorZhihu = post.zhihu_addr;
+            var authorWechat = post.wechat_image;
+            var authorStackoverflow = "";
+
+            iframes += `<td align="center" style="text-align:center;border: 1px solid #c1cfdc;color: #${font_color};font-weight: ${font_weight};width: ${board_width}; padding: 10px 10px;">`;
+            iframes += `<a href="${authorLink}"><img src="${authorAvatar}" width="${image_size};" alt="" style="display: block;margin: 0 auto;padding: 2px;max-width: 96px;height: auto;border: 2px solid #333;border-radius: ${image_size};"/>`;
+            iframes += `<sub text-align="center" style="font-size: 15px;">${authorName}</sub></a><br/>`;
+            if(!(githubName == null || githubName === ""))
+                iframes += `<a href="https://github.com/${githubName}" title="Github"><i class="fab fa-github fa-fw" style="color:#${font_icon_color}"></i></a>`;
+            if(!(authorZhihu == null || authorZhihu === ""))
+                iframes += `<a href="${authorZhihu}" title="知乎"><i class="fa fa-custom zhihu fa-fw" style="color:#${font_icon_color}"></i></a>`;
+            if(!(authorZhihu == null || authorZhihu === ""))
+                iframes += `<a href="${authorWechat}" title="微信"><i class="fab fa-weixin fa-fw" style="color:#${font_icon_color}"></i></a>`;
+            if(!(authorStackoverflow == null || authorStackoverflow === ""))
+                iframes += `<a href="${authorStackoverflow}" title="stack overflow"><i class="fab fa-stack-overflow" style="color:#${font_icon_color}"></i></a>`;
+            if(!(authorEmail == null || authorEmail === ""))
+                iframes += `<a href="mailto:${authorEmail}" title="Email"><i class="fa fa-envelope fa-fw" style="color:#${font_icon_color}"></i></a>`;
+            if(!(article_num == null || article_num === 0))
+            {
+                iframes += `<br/>`;
+                iframes += `<sub text-align="center" style="font-size: 14px;color: #555";buttom: 0;>发表 ${article_num} 篇内容</sub></a>`;
+            }
+        });
+        iframes += `</table></tr>`;
+   });
+    return iframes;
+},{ async: true });
